e7d71bc8a9062b03551e581dced346820e3f582b
Carson M <eclipticttv@gmail.com>
2022-02-15 12:31:01 -0600

    Remove templates from GLIDE gaussian diffusion

--- src/libglide/models/glide/GaussianDiffusion.cc
+++ src/libglide/models/glide/GaussianDiffusion.cc
@@ -48,8 +48,7 @@ namespace {
 	 * @param broadcastShape a larger shape of K dimensions with the batch dimension equal to the length of timesteps
 	 * @returns a tensor of shape [batchSize, 1, ...] where the shape has K dims.
 	 */
-	template<typename T = double>
-	xt::xarray<T> _extractIntoTensor(const xt::xtensor<T, 1> arr, const xt::xtensor<int, 1> timesteps, const std::array<size_t, 4> broadcastShape) {
+	xt::xarray<double> _extractIntoTensor(const xt::xtensor<double, 1> arr, const xt::xtensor<int, 1> timesteps, const std::array<size_t, 4> broadcastShape) {
 		// res = th.from_numpy(arr).to(device=timesteps.device)[timesteps].float()
 		// while len(res.shape) < len(broadcast_shape):
 		// 	res = res[..., None]
@@ -58,14 +57,13 @@ namespace {
 		// if timesteps is of length 2 (as the case of the GLIDE main model), we need to make sure that both timesteps are identical
 		// otherwise this very, very hacky code will break
 		assert(timesteps.size() > 1 ? timesteps[0] == timesteps[1] : true);
-		return arr.flat(timesteps[0] /* this is gonna fucking break */) * xt::ones<T>(broadcastShape);
+		return arr.flat(timesteps[0] /* this is gonna fucking break */) * xt::ones<double>(broadcastShape);
 	}
 }
 
-template<typename T = double>
 class GaussianDiffusion {
 	public:
-		GaussianDiffusion(xt::xtensor<T, 1> betas) : betas(betas) {
+		GaussianDiffusion(xt::xtensor<double, 1> betas) : betas(betas) {
 			assert(betas.shape().size() == 1);
 			assert(xt::all(betas > 0 && betas <= 1));
 
@@ -75,12 +73,12 @@ class GaussianDiffusion {
 			//std::cout << this->numTimesteps << std::endl;
 
 			auto alphas = 1.0 - betas;
-			this->alphasCumulative = xt::cumprod<T>(xt::eval(alphas), 0);
+			this->alphasCumulative = xt::cumprod<double>(xt::eval(alphas), 0);
 			assert(this->alphasCumulative.shape()[0] == this->numTimesteps);
 			// self.alphas_cumprod_prev = np.append(1.0, self.alphas_cumprod[:-1])
-			this->alphasCumulativePrev = xt::concatenate(xt::xtuple(xt::xtensor<T, 1>({ 1.0 }), xt::view(this->alphasCumulative, xt::range(0, -1))));
+			this->alphasCumulativePrev = xt::concatenate(xt::xtuple(xt::xtensor<double, 1>({ 1.0 }), xt::view(this->alphasCumulative, xt::range(0, -1))));
 			// self.alphas_cumprod_next = np.append(self.alphas_cumprod[1:], 0.0)
-			this->alphasCumulativeNext = xt::concatenate(xt::xtuple(xt::view(this->alphasCumulative, xt::range(1, xt::placeholders::_)), xt::xtensor<T, 1>({ 0.0 })));
+			this->alphasCumulativeNext = xt::concatenate(xt::xtuple(xt::view(this->alphasCumulative, xt::range(1, xt::placeholders::_)), xt::xtensor<double, 1>({ 0.0 })));
 			assert(this->alphasCumulative.shape()[0] == this->alphasCumulativePrev.shape()[0]);
 
 			//std::cout << this->alphasCumulative << std::endl;
@@ -104,7 +102,7 @@ class GaussianDiffusion {
 			// calculations for posterior q(x_{t-1} | x_t, x_0)
 			this->posteriorVariance = (betas * (1.0 - this->alphasCumulativePrev) / (1.0 - this->alphasCumulative));
 			// below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
-			this->posteriorLogVarianceClipped = xt::log(xt::concatenate(xt::xtuple(xt::xtensor<T, 1>({ this->posteriorVariance[1] }), xt::view(this->posteriorVariance, xt::range(1, xt::placeholders::_)))));
+			this->posteriorLogVarianceClipped = xt::log(xt::concatenate(xt::xtuple(xt::xtensor<double, 1>({ this->posteriorVariance[1] }), xt::view(this->posteriorVariance, xt::range(1, xt::placeholders::_)))));
 			this->posteriorMeanCoef1 = (betas * xt::sqrt(this->alphasCumulativePrev) / (1.0 - this->alphasCumulative));
 			this->posteriorMeanCoef2 = ((1.0 - this->alphasCumulativePrev) * xt::sqrt(alphas) / (1.0 - this->alphasCumulative));
 
@@ -119,7 +117,7 @@ class GaussianDiffusion {
 		 *
 		 *    q(x_{t-1} | x_t, x_0)
 		 */
-		auto qPosteriorMeanVariance(xt::xtensor<T, 4> xStart, xt::xtensor<T, 4> x_t, xt::xtensor<T, 1> t) {
+		auto qPosteriorMeanVariance(xt::xtensor<double, 4> xStart, xt::xtensor<double, 4> x_t, xt::xtensor<double, 1> t) {
 			assert(xStart.shape() == x_t.shape());
 			auto posteriorMean = (
 				  _extractIntoTensor(this->posteriorMeanCoef1, t, x_t.shape()) * xStart
@@ -149,10 +147,10 @@ class GaussianDiffusion {
 		template<typename Model, typename ...ModelArgs>
 		auto pMeanVariance(
 			Model const &model,
-			xt::xtensor<T, 4> x,
+			xt::xtensor<double, 4> x,
 			xt::xtensor<int, 1> t,
 			bool clipDenoised = true,
-			std::function<xt::xtensor<T, 4>(xt::xtensor<T, 4>)> denoisedFn = nullptr,
+			std::function<xt::xtensor<double, 4>(xt::xtensor<double, 4>)> denoisedFn = nullptr,
 			ModelArgs&&... modelArgs
 		) {
 			auto B /* batches? */ = x.shape()[0];
@@ -190,7 +188,7 @@ class GaussianDiffusion {
 		}
 
 	protected:
-		auto predictXStartFromEps(xt::xtensor<T, 4> x, xt::xtensor<int, 1> t, xt::xtensor<T, 4> eps) {
+		auto predictXStartFromEps(xt::xtensor<double, 4> x, xt::xtensor<int, 1> t, xt::xtensor<double, 4> eps) {
 			assert(x.shape() == eps.shape());
 			return xt::eval(
 				  _extractIntoTensor(this->sqrtRecipAlphasCumulative, t, x.shape()) * x
@@ -198,34 +196,32 @@ class GaussianDiffusion {
 			);
 		}
 
-		auto predictEpsFromXStart(xt::xtensor<T, 4> x, xt::xtensor<int, 1> t, xt::xtensor<T, 4> predXStart) {
+		auto predictEpsFromXStart(xt::xtensor<double, 4> x, xt::xtensor<int, 1> t, xt::xtensor<double, 4> predXStart) {
 			return xt::eval(
 				 (_extractIntoTensor(this->sqrtRecipAlphasCumulative, t, x.shape()) * x - predXStart)
 				/ _extractIntoTensor(this->sqrtRecipm1AlphasCumulative, t, x.shape())
 			);
 		}
 
-		xt::xtensor<T, 1> betas;
+		xt::xtensor<double, 1> betas;
 		int numTimesteps;
 
 		// TODO: these should be xfunction for optimizations
-		xt::xtensor<T, 1> alphasCumulative;
-		xt::xtensor<T, 1> alphasCumulativePrev;
-		xt::xtensor<T, 1> alphasCumulativeNext;
+		xt::xtensor<double, 1> alphasCumulative;
+		xt::xtensor<double, 1> alphasCumulativePrev;
+		xt::xtensor<double, 1> alphasCumulativeNext;
 
-		xt::xtensor<T, 1> sqrtAlphasCumulative;
-		xt::xtensor<T, 1> sqrtOneMinusAlphasCumulative;
-		xt::xtensor<T, 1> sqrtLogOneMinusAlphasCumulative;
-		xt::xtensor<T, 1> sqrtRecipAlphasCumulative;
-		xt::xtensor<T, 1> sqrtRecipm1AlphasCumulative;
+		xt::xtensor<double, 1> sqrtAlphasCumulative;
+		xt::xtensor<double, 1> sqrtOneMinusAlphasCumulative;
+		xt::xtensor<double, 1> sqrtLogOneMinusAlphasCumulative;
+		xt::xtensor<double, 1> sqrtRecipAlphasCumulative;
+		xt::xtensor<double, 1> sqrtRecipm1AlphasCumulative;
 
-		xt::xtensor<T, 1> posteriorVariance;
-		xt::xtensor<T, 1> posteriorLogVarianceClipped;
-		xt::xtensor<T, 1> posteriorMeanCoef1;
-		xt::xtensor<T, 1> posteriorMeanCoef2;
+		xt::xtensor<double, 1> posteriorVariance;
+		xt::xtensor<double, 1> posteriorLogVarianceClipped;
+		xt::xtensor<double, 1> posteriorMeanCoef1;
+		xt::xtensor<double, 1> posteriorMeanCoef2;
 };
-
-template class GaussianDiffusion<double>;
 
 }
 
@@ -237,7 +233,7 @@ template class GaussianDiffusion<double>;
 
 // 	for (int i = 0; i < 1; i++) {
 // 		auto betas = glide::getNamedBetaSchedule(glide::BetaScheduleTypeV2::BETA_SCHEDULE_SQUAREDCOS_CAP_V2, 1000);
-// 		glide::GaussianDiffusion<double> gd(betas);
+// 		glide::GaussianDiffusion gd(betas);
 // 		gd.pMeanVariance(
 // 			[](xt::xtensor<double, 4> x, xt::xtensor<int, 1> t) {
 // 				return xt::eval(xt::concatenate(xt::xtuple(x, x), 1));
