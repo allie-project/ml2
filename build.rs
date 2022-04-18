d7d0a266b2107fe98e17772b94a22bd4e5a51314
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-04-18 15:19:58 -0500

    build: [wip] build onnxruntime from source

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::{
 	env, fs,
 	io::{self, Read, Write},
 	path::{Path, PathBuf},
+	process::Stdio,
 	str::FromStr
 };
 
@@ -10,7 +11,14 @@ const ORT_VERSION: &str = "1.11.0";
 const ORT_RELEASE_BASE_URL: &str = "https://github.com/microsoft/onnxruntime/releases/download";
 const ORT_ENV_STRATEGY: &str = "ORT_STRATEGY";
 const ORT_ENV_SYSTEM_LIB_LOCATION: &str = "ORT_LIB_LOCATION";
-const ORT_PREBUILD_EXTRACT_DIR: &str = "onnxruntime";
+const ORT_ENV_CMAKE_TOOLCHAIN: &str = "ORT_CMAKE_TOOLCHAIN";
+const ORT_ENV_CMAKE_PROGRAM: &str = "ORT_CMAKE_PROGRAM";
+const ORT_EXTRACT_DIR: &str = "onnxruntime";
+const ORT_GIT_DIR: &str = "onnxruntime";
+const ORT_GIT_REPO: &str = "https://github.com/microsoft/onnxruntime";
+const PROTOBUF_EXTRACT_DIR: &str = "protobuf";
+const PROTOBUF_VERSION: &str = "3.11.2";
+const PROTOBUF_RELEASE_BASE_URL: &str = "https://github.com/protocolbuffers/protobuf/releases/download";
 
 trait OnnxPrebuiltArchive {
 	fn as_onnx_str(&self) -> Cow<str>;
@@ -138,7 +146,7 @@ impl OnnxPrebuiltArchive for Triplet {
 	}
 }
 
-fn prebuilt_archive_url() -> (PathBuf, String) {
+fn prebuilt_onnx_url() -> (PathBuf, String) {
 	let accelerator = if cfg!(feature = "cuda") { Accelerator::Gpu } else { Accelerator::None };
 
 	let triplet = Triplet {
@@ -149,6 +157,30 @@ fn prebuilt_archive_url() -> (PathBuf, String) {
 
 	let prebuilt_archive = format!("onnxruntime-{}-{}.{}", triplet.as_onnx_str(), ORT_VERSION, triplet.os.archive_extension());
 	let prebuilt_url = format!("{}/v{}/{}", ORT_RELEASE_BASE_URL, ORT_VERSION, prebuilt_archive);
+
+	(PathBuf::from(prebuilt_archive), prebuilt_url)
+}
+
+fn prebuilt_protoc_url() -> (PathBuf, String) {
+	let host_platform = if cfg!(target_os = "windows") {
+		std::string::String::from("win32")
+	} else if cfg!(target_os = "macos") {
+		format!(
+			"osx-{}",
+			if cfg!(target_arch = "x86_64") {
+				"x86_64"
+			} else if cfg!(target_arch = "x86") {
+				"x86"
+			} else {
+				panic!("protoc does not have prebuilt binaries for darwin arm64 yet")
+			}
+		)
+	} else {
+		format!("linux-{}", if cfg!(target_arch = "x86_64") { "x86_64" } else { "x86_32" })
+	};
+
+	let prebuilt_archive = format!("protoc-{}-{}.zip", PROTOBUF_VERSION, host_platform);
+	let prebuilt_url = format!("{}/v{}/{}", PROTOBUF_RELEASE_BASE_URL, PROTOBUF_VERSION, prebuilt_archive);
 
 	(PathBuf::from(prebuilt_archive), prebuilt_url)
 }
@@ -177,7 +209,6 @@ where
 
 fn extract_archive(filename: &Path, output: &Path) {
 	match filename.extension().map(|e| e.to_str()) {
-		#[cfg(target_os = "windows")]
 		Some(Some("zip")) => extract_zip(filename, output),
 		#[cfg(not(target_os = "windows"))]
 		Some(Some("tgz")) => extract_tgz(filename, output),
@@ -194,7 +225,6 @@ fn extract_tgz(filename: &Path, output: &Path) {
 	archive.unpack(output).unwrap();
 }
 
-#[cfg(target_os = "windows")]
 fn extract_zip(filename: &Path, outpath: &Path) {
 	let file = fs::File::open(&filename).unwrap();
 	let buf = io::BufReader::new(file);
@@ -221,10 +251,10 @@ fn prepare_libort_dir() -> PathBuf {
 	println!("[glide] strategy: {:?}", strategy.as_ref().map(String::as_str).unwrap_or_else(|_| "unknown"));
 	match strategy.as_ref().map(String::as_str) {
 		Ok("download") | Err(_) => {
-			let (prebuilt_archive, prebuilt_url) = prebuilt_archive_url();
+			let (prebuilt_archive, prebuilt_url) = prebuilt_onnx_url();
 
 			let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-			let extract_dir = out_dir.join(ORT_PREBUILD_EXTRACT_DIR);
+			let extract_dir = out_dir.join(ORT_EXTRACT_DIR);
 			let downloaded_file = out_dir.join(&prebuilt_archive);
 
 			println!("cargo:rerun-if-changed={}", downloaded_file.display());
@@ -246,7 +276,80 @@ fn prepare_libort_dir() -> PathBuf {
 				panic!("[glide] system strategy requires ORT_LIB_LOCATION env var to be set: {}", e);
 			}
 		}),
-		Ok("compile") => unimplemented!(),
+		Ok("compile") => {
+			use std::process::Command;
+
+			let target = env::var("TARGET").unwrap();
+			if target.contains("macos") && !cfg!(target_os = "darwin") && env::var(ORT_ENV_CMAKE_PROGRAM).is_err() {
+				panic!("[glide] cross-compiling for macOS with the `compile` strategy requires `{}` to be set", ORT_ENV_CMAKE_PROGRAM);
+			}
+
+			let cmake = env::var(ORT_ENV_CMAKE_PROGRAM).unwrap_or_else(|_| "cmake".to_string());
+			let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+			let required_cmds: &[&str] = &[&cmake, "python", "git"];
+			for cmd in required_cmds {
+				if Command::new(cmd).output().is_err() {
+					panic!("[glide] compile strategy requires `{}` to be installed", cmd);
+				}
+			}
+
+			println!("[glide] assuming C/C++ compilers are available");
+
+			Command::new("git")
+				.args(&["clone", "--depth", "1", "--recursive", ORT_GIT_REPO, ORT_GIT_DIR])
+				.current_dir(&out_dir)
+				.stdout(Stdio::null())
+				.stderr(Stdio::null())
+				.status()
+				.expect("failed to clone ORT repo");
+
+			// download prebuilt protoc binary
+			let (protoc_archive, protoc_url) = prebuilt_protoc_url();
+			let protoc_dir = out_dir.join(PROTOBUF_EXTRACT_DIR);
+			let protoc_archive_file = out_dir.join(&protoc_archive);
+
+			println!("cargo:rerun-if-changed={}", protoc_archive_file.display());
+
+			if !protoc_archive_file.exists() {
+				download(&protoc_url, &protoc_archive_file);
+			}
+
+			if !protoc_dir.exists() {
+				extract_archive(&protoc_archive_file, &protoc_dir);
+			}
+
+			let protoc_file = if cfg!(target_os = "windows") { "protoc.exe" } else { "protoc" };
+			let protoc_file = protoc_dir.join("bin").join(protoc_file);
+
+			Command::new(protoc_file)
+				.args(&["--help"])
+				.current_dir(&out_dir)
+				.stdout(Stdio::null())
+				.status()
+				.expect("error running `protoc --help`");
+
+			let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+			let cmake_toolchain = env::var(ORT_ENV_CMAKE_TOOLCHAIN).map(PathBuf::from).unwrap_or(
+				if cfg!(target_os = "linux") && target.contains("aarch64") && target.contains("linux") {
+					root.join("toolchains").join("default-aarch64-linux-gnu.cmake")
+				} else if cfg!(target_os = "linux") && target.contains("aarch64") && target.contains("windows") {
+					root.join("toolchains").join("default-aarch64-w64-mingw32.cmake")
+				} else if cfg!(target_os = "linux") && target.contains("x86_64") && target.contains("windows") {
+					root.join("toolchains").join("default-x86_64-w64-mingw32.cmake")
+				} else {
+					PathBuf::new()
+				}
+			);
+
+			if cfg!(target_os = "linux") && target.contains("windows") && target.contains("aarch64") {
+				println!("[glide] detected cross compilation to Windows arm64, default toolchain will make bad assumptions.");
+			}
+
+			// TODO
+			panic!("[glide] compile strategy not implemented");
+
+			out_dir
+		}
 		_ => panic!("[glide] unknown strategy: {} (valid options are `download` or `system`)", strategy.unwrap_or_else(|_| "unknown".to_string()))
 	}
 }
