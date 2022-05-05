e464029aaa4289fb407ed07573f34e02e8a84c20
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-05-04 20:44:17 -0500

    feat: naive bayes

---
+++ src/bayes/mod.rs
@@ -0,0 +1,11 @@
+mod base_nb;
+mod error;
+mod gaussian_nb;
+mod hyperparams;
+mod multinomial_nb;
+
+pub use error::{NaiveBayesError, Result};
+pub use gaussian_nb::GaussianNb;
+pub use hyperparams::{GaussianNbParams, GaussianNbValidParams};
+pub use hyperparams::{MultinomialNbParams, MultinomialNbValidParams};
+pub use multinomial_nb::MultinomialNb;
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 extern crate blas_src;
 
 pub mod audio;
+#[cfg(feature = "bayes")]
+pub mod bayes;
 pub mod core;
 #[cfg(all(feature = "_linalg", feature = "logistic"))]
 pub mod logistic;
