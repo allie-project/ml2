ac756f370d6bdaae02508a06b5ae76a5c2edf6ec
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-06-16 17:27:38 -0500

    fix(onnx): `metadata.custom()` returns None if key doesnt exist

--- src/onnx/metadata.rs
+++ src/onnx/metadata.rs
@@ -1,5 +1,10 @@
 #![allow(clippy::tabs_in_doc_comments)]
 
+use std::{
+	ffi::{c_void, CString},
+	os::raw::c_char
+};
+
 use super::{
 	char_p_to_string,
 	error::{assert_non_null_pointer, status_to_result, Result as OrtResult},
@@ -54,15 +59,20 @@ impl Metadata {
 		Ok(out_ver)
 	}
 
-	pub fn custom(&self, key: &str) -> OrtResult<String> {
-		let mut out_str: *mut i8 = std::ptr::null_mut();
-		let status =
-			unsafe { ort().ModelMetadataLookupCustomMetadataMap.unwrap()(self.metadata_ptr, self.allocator_ptr, key.as_ptr() as *const i8, &mut out_str) };
+	pub fn custom(&self, key: &str) -> OrtResult<Option<String>> {
+		let mut out_str: *mut c_char = std::ptr::null_mut();
+		let key_str = CString::new(key)?;
+		let status = unsafe { ort().ModelMetadataLookupCustomMetadataMap.unwrap()(self.metadata_ptr, self.allocator_ptr, key_str.as_ptr(), &mut out_str) };
 		status_to_result(status).map_err(OrtError::Allocator)?;
-		assert_non_null_pointer(out_str, "ModelMetadataLookupCustomMetadataMap")?;
-
-		let out_str = char_p_to_string(out_str)?;
-		Ok(out_str)
+		if !out_str.is_null() {
+			unsafe {
+				let value = char_p_to_string(out_str)?;
+				(*self.allocator_ptr).Free.unwrap()(self.allocator_ptr, out_str as *mut c_void);
+				Ok(Some(value))
+			}
+		} else {
+			Ok(None)
+		}
 	}
 }
 
